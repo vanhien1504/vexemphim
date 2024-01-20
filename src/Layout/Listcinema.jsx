14afbcdb9652e51251578debad32844c85b84c5d
Linh <quanglinhle2102@gmail.com>
2024-01-20 21:07:06 +0700

    list

---
+++ src/Layout/Listcinema.jsx
@@ -0,0 +1,176 @@
+import React from 'react';
+
+
+const Listcinema = () => {
+    return (
+        <div className="container mx-auto row row-small">
+        <h2 className=" text-center font-bold " center>Hệ Thống Phim</h2>
+        <div className="d-flex grid grid-cols-2 sm:grid-cols-6 md:grid-cols-4 lg:grid-cols-4 gap-4 py-5">
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t "
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg  text-stone-50 ">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+        </div>
+        <div className="d-flex grid grid-cols-2 sm:grid-cols-6 md:grid-cols-4 lg:grid-cols-4 gap-4 py-2">
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+        </div>
+        <div className="d-flex grid grid-cols-2 sm:grid-cols-6 md:grid-cols-4 lg:grid-cols-4 gap-4 py-2
+        ">
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+                <div className="bg-gray rounded shadow">
+                    <img
+                        className="w-full h-64 object-cover rounded-t"
+                        src="/images/cinema.jpg"
+                        alt=""
+                    />
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold mb-2">Tên phim</h3>
+                        <button className=" btn btn-secondary btn-lg btn-lg text-stone-50">Thông tin Chi Tiết</button>
+                        <button className=" btn btn-light btn-lg text-zinc-800">Trailer</button>
+
+                    </div>
+                </div>
+        </div>
+        
+      </div>   
+    );
+  };
+  
+  export default Listcinema;
