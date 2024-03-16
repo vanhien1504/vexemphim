74c2860e13ca03fc274a1c9b1091b587cded5f88
Linh <quanglinhle2102@gmail.com>
2024-03-16 14:32:53 +0700

    lis

--- src/Layout/Listcinema.jsx
+++ src/Layout/Listcinema.jsx
@@ -3,136 +3,73 @@ import React from 'react';
 
 const Listcinema = () => {
     return (
-        <div className="bg-neutral-600">
-        <div className="container mx-auto row row-small">
-            <h2 className=" text-center font-bold text-stone-50 py-4" center>Lịch Chiếu </h2>
-            <div className="d-flex grid grid-cols-2 sm:grid-cols-6 md:grid-cols-4 lg:grid-cols-4 gap-4 px-0 py-4">
-                <div className="bg-gray rounded shadow ">
+        <div className=" bg-slate-600">
+            <h1 class=" text-4xl text-white  text-center mb-8">Hệ thống rạp</h1>
+
+            <div class="container mx-auto px-4 py-8">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div class=" rounded-lg shadow-md p-4">
                 <img
                     className="w-full object-cover rounded-t "
                     src="/images/cinema.jpg"
                     alt=""
                 />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] mx-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
+                <p class="text-lg font-bold mb-2">Rạp 1</p>
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded mt-2"
+                  style={{ marginRight: '8px' }}
+                >
+                  Thông tin chi tiết
 
-                </div>
+                </button>
             </div>
-            <div className="bg-gray rounded shadow">
+            <div class=" rounded-lg shadow-md p-4">
                 <img
                     className="w-full object-cover rounded-t "
                     src="/images/cinema.jpg"
                     alt=""
                 />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] m-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
+                                <p class="text-lg font-bold mb-2">Rạp 2</p>
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded mt-2"
+                  style={{ marginRight: '8px' }}
+                >
+                  Thông tin chi tiết
 
-                </div>
+                </button>
             </div>
-            <div className="bg-gray rounded shadow">
+            <div class=" rounded-lg shadow-md p-4">
                 <img
                     className="w-full object-cover rounded-t "
                     src="/images/cinema.jpg"
                     alt=""
                 />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] mx-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
+                     <p class="text-lg font-bold mb-2">Rạp 3</p>
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded mt-2"
+                  style={{ marginRight: '8px' }}
+                >
+                  Thông tin chi tiết
 
-                </div>
+                </button>
             </div>
-            <div className="bg-gray rounded shadow">
-                <img
-                    className="w-full  object-cover rounded-t "
-                    src="/images/cinema.jpg"
-                    alt=""
-                />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] mx-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
-
-                </div>
-            </div>
-            </div>
-            <div className="d-flex grid grid-cols-2 sm:grid-cols-6 md:grid-cols-4 lg:grid-cols-4 gap-4 px-0 py-4">
-                <div className="bg-gray rounded shadow ">
+            <div class=" rounded-lg shadow-md p-4">
                 <img
                     className="w-full object-cover rounded-t "
                     src="/images/cinema.jpg"
                     alt=""
                 />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] mx-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
-
-                </div>
+                <p class="text-lg font-bold mb-2">Rạp 4</p>
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded mt-2"
+                  style={{ marginRight: '8px' }}
+                >
+                  Thông tin chi tiết
+                </button>
             </div>
-            <div className="bg-gray rounded shadow">
-                <img
-                    className="w-full object-cover rounded-t "
-                    src="/images/cinema.jpg"
-                    alt=""
-                />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] m-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
-
-                </div>
+        </div>
             </div>
-            <div className="bg-gray rounded shadow">
-                <img
-                    className="w-full object-cover rounded-t "
-                    src="/images/cinema.jpg"
-                    alt=""
-                />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] mx-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
-
-                </div>
-            </div>
-            <div className="bg-gray rounded shadow">
-                <img
-                    className="w-full object-cover rounded-t "
-                    src="/images/cinema.jpg"
-                    alt=""
-                />
-                <div className="p-4">
-                    <h3 className="text-lg font-bold mb-2 text-stone-50">Tên Rạp</h3>
-                    <div className="d-flex">
-                    <button className="bg-zinc-500 text-stone-50 text-[18px] w-[170px] h-10 rounded-[5px] mx-2">Thông tin Chi Tiết</button>
-                    <button className=" w-[80px] h-10 relative bg-stone-50 rounded-[5px] border border-zinc-800 text-zinc-800 text-[17px]">Trailer</button>
-                    </div>
-
-                </div>
-            </div>
-            </div>
-            
-
-        </div>   
+        
     </div>
     );
   };
